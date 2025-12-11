6dccda610183c3de0b26472c9415c8af382df66f
Siddharth Rao <siddrao.926@gmail.com>
2025-12-11 05:40:25 -0800

    Changed Full Sync Schedule

--- apps/data/main.py
+++ apps/data/main.py
@@ -9,7 +9,7 @@ def main():
     full_sync()
 
     full_sync_deployment = full_sync.to_deployment(
-        name="full-sync-deployment", cron="0 0 * * 1,3,5"
+        name="full-sync-deployment", cron="0 0 * * 2,5"
     )
 
     live_sync_deployment = live_sync.to_deployment(
