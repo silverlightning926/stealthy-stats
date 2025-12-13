46fe7a6f1591fc4829a0fe09a3cf6baed5673241
Siddharth Rao <siddrao.926@gmail.com>
2025-12-13 14:45:41 -0800

    Added Event Teams To Initial Full Sync Check and Changed Full Sync Schedule

--- apps/data/main.py
+++ apps/data/main.py
@@ -1,7 +1,9 @@
 from datetime import timedelta
 
 from prefect import serve
+from sqlalchemy import func, select
 
+from app.models.tba import Alliance, Event, EventTeam, Match, Ranking, Team
 from app.pipeline.flows import full_sync, live_sync
 from app.services import DBService
 
@@ -10,20 +12,20 @@ def main():
     db = DBService()
     try:
         with db.get_session() as session:
-            from sqlmodel import func, select
-
-            from app.models.tba import Alliance, Event, Match, Ranking, Team
-
-            team_count = session.exec(select(func.count(Team.key))).one()  # pyright: ignore[reportArgumentType]
-            event_count = session.exec(select(func.count(Event.key))).one()  # pyright: ignore[reportArgumentType]
-            match_count = session.exec(select(func.count(Match.key))).one()  # pyright: ignore[reportArgumentType]
-            ranking_count = session.exec(select(func.count(Ranking.event_key))).one()  # pyright: ignore[reportArgumentType]
-            alliance_count = session.exec(select(func.count(Alliance.event_key))).one()  # pyright: ignore[reportArgumentType]
+            team_count = session.scalar(select(func.count()).select_from(Team))
+            event_count = session.scalar(select(func.count()).select_from(Event))
+            event_team_count = session.scalar(
+                select(func.count()).select_from(EventTeam)
+            )
+            match_count = session.scalar(select(func.count()).select_from(Match))
+            ranking_count = session.scalar(select(func.count()).select_from(Ranking))
+            alliance_count = session.scalar(select(func.count()).select_from(Alliance))
 
             if any(
                 [
                     team_count == 0,
                     event_count == 0,
+                    event_team_count == 0,
                     match_count == 0,
                     ranking_count == 0,
                     alliance_count == 0,
@@ -35,11 +37,13 @@ def main():
         full_sync()
 
     full_sync_deployment = full_sync.to_deployment(
-        name="full-sync-deployment", cron="0 0 * * 2,5"
+        name="full-sync-deployment",
+        cron="0 4 * * 1,4",
     )
 
     live_sync_deployment = live_sync.to_deployment(
-        name="live-sync-deployment", interval=timedelta(minutes=10)
+        name="live-sync-deployment",
+        interval=timedelta(minutes=10),
     )
 
     serve(full_sync_deployment, live_sync_deployment)  # type: ignore
