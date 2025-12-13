af7a21ff7d55f26e6275cf27c7e0eb229a30aa11
Siddharth Rao <siddrao.926@gmail.com>
2025-12-13 04:05:39 -0800

    Added Checks For Initial Full Sync To Limit Redundant Runs

--- apps/data/main.py
+++ apps/data/main.py
@@ -3,10 +3,38 @@ from datetime import timedelta
 from prefect import serve
 
 from app.pipeline.flows import full_sync, live_sync
+from app.services import DBService
 
 
 def main():
-    full_sync()
+    db = DBService()
+    try:
+        with db.get_session() as session:
+            from sqlmodel import func, select
+
+            from app.models.tba import Alliance, District, Event, Match, Ranking, Team
+
+            team_count = session.exec(select(func.count(Team.key))).one()  # pyright: ignore[reportArgumentType]
+            event_count = session.exec(select(func.count(Event.key))).one()  # pyright: ignore[reportArgumentType]
+            district_count = session.exec(select(func.count(District.key))).one()  # pyright: ignore[reportArgumentType]
+            match_count = session.exec(select(func.count(Match.key))).one()  # pyright: ignore[reportArgumentType]
+            alliance_count = session.exec(select(func.count(Alliance.event_key))).one()  # pyright: ignore[reportArgumentType]
+            ranking_count = session.exec(select(func.count(Ranking.event_key))).one()  # pyright: ignore[reportArgumentType]
+
+            if any(
+                [
+                    team_count == 0,
+                    event_count == 0,
+                    district_count == 0,
+                    match_count == 0,
+                    alliance_count == 0,
+                    ranking_count == 0,
+                ]
+            ):
+                full_sync()
+
+    except Exception:
+        full_sync()
 
     full_sync_deployment = full_sync.to_deployment(
         name="full-sync-deployment", cron="0 0 * * 2,5"
